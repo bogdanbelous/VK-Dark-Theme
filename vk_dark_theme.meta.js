179f7a335b89aef8067e28f58fd94b2e7c0dce8f
Bogdan Belous <42302466+bogdanbelous@users.noreply.github.com>
2018-08-13 20:28:51 +0300

    Create vk_dark_theme.meta.js

---
+++ vk_dark_theme.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         VK Dark Theme
+// @version      0.1
+// @author       bogdanbelous
+// @namespace    https://github.com/bogdanbelous/VK-Dark-Theme/blob/master/README.md
+// @description  :)
+// @homepage     https://github.com/bogdanbelous/VK-Dark-Theme/
+// @include      https://vk.com/*
+// @include      http://vk.com/*
+// @run-at       document-start
+// @downloadURL  https://raw.githubusercontent.com/bogdanbelous/VK-Dark-Theme/master/vk_dark_theme.js
+// @updateURL    https://raw.githubusercontent.com/bogdanbelous/VK-Dark-Theme/master/vk_dark_theme.js
+// ==/UserScript==
