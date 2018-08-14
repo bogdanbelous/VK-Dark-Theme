6d6b781c9e5c929eab6d55453036acc1b38a3924
Bogdan Belous <42302466+bogdanbelous@users.noreply.github.com>
2018-08-14 14:00:05 +0300

    Update vk_dark_theme.meta.js

--- vk_dark_theme.meta.js
+++ vk_dark_theme.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         VK Dark Theme
-// @version      0.1
+// @version      0.1.1
 // @author       bogdanbelous
 // @description  Enjoy it
 // @namespace    https://github.com/bogdanbelous/VK-Dark-Theme
