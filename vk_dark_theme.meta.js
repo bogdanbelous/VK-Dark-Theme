5c9b6edf30f35eb79b927c30647a70199266062b
bogdanbelous <belousb1@gmail.com>
2018-08-17 13:53:07 +0300

    version 0.1.4

--- vk_dark_theme.meta.js
+++ vk_dark_theme.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         VK Dark Theme
-// @version      0.1.3
+// @version      0.1.4
 // @author       bogdanbelous
 // @description  Enjoy it
 // @namespace    https://github.com/bogdanbelous/VK-Dark-Theme
