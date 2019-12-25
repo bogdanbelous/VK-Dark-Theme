363d1c8dd69e6f2f3ed1b437e147b20f48fb901c
Bogdan Belous <belousb1@gmail.com>
2019-12-25 14:25:42 +0200

    Add files via upload

--- vk_dark_theme.meta.js
+++ vk_dark_theme.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         VK Dark Theme
-// @version      0.2.3
+// @version      0.2.4
 // @author       bogdanbelous
 // @description  Enjoy it
 // @namespace    https://github.com/bogdanbelous/VK-Dark-Theme
