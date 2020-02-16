c65046ecdafc82b7ac7270c764006782cddd34e0
Bogdan Belous <belousb1@gmail.com>
2020-02-16 20:55:22 +0200

    version 0.2.6

--- vk_dark_theme.meta.js
+++ vk_dark_theme.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         VK Dark Theme
-// @version      0.2.5
+// @version      0.2.6
 // @author       bogdanbelous
 // @description  Enjoy it
 // @namespace    https://github.com/bogdanbelous/VK-Dark-Theme
